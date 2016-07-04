276cc8c0ba3ab09ede044a27a61114b299897677
Wouter Vandenneucker <wouter@vandenneucker.be>
2016-07-04 16:10:37 +0200

    [WIP] change the cors settings and branding of the plugin

--- ext-src/js/login.js
+++ ext-src/js/login.js
@@ -1,27 +1,28 @@
 name = localStorage["login"];
 pass = localStorage["pass"];
 
-url = "https://uhasselt-wifi.uhasselt.be/cgi-bin/login";
-data = "user="+name+"&password="+pass+"&cmd=authenticate&login=Log In";
+url = "https://1.1.1.1/login.html";
+//data = "user="+name+"&password="+pass+"&cmd=authenticate&login=Log In";
+data = "buttonClicked=4&err_flag=0&err_msg=&info_flag=0&info_msg=&network_name=Guest+Network&username="+name+"&password="+pass;
 
-window.alert(data);
+//window.alert(data);
 loadXMLDoc();
 response;
 function loadXMLDoc(){
     var xmlhttp;
-    if (window.XMLHttpRequest){
+    //if (window.XMLHttpRequest){
       // code for IE7+, Firefox, Chrome, Opera, Safari
       xmlhttp=new XMLHttpRequest();
-    }
-    else{
+    //}
+    //else{
       // code for IE6, IE5
-      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    
+      //xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    //}
+
     xmlhttp.open("POST",url,false);
-    //xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     xmlhttp.send(data);
     //document.getElementById("main").innerHTML = xmlhttp.responseText;
-    //response = xmlhttp.responseText;
-    //console.log(response);
+    response = xmlhttp.responseText;
+    console.log(response);
 }
