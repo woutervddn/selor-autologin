07e4e003c84b09ff9d9a590ba6ae859a91cdda6f
Wouter Vandenneucker <wouter@vandenneucker.be>
2016-07-05 08:37:12 +0200

    make final changes to get this thing working

--- ext-src/js/login.js
+++ ext-src/js/login.js
@@ -2,27 +2,21 @@ name = localStorage["login"];
 pass = localStorage["pass"];
 
 url = "https://1.1.1.1/login.html";
-//data = "user="+name+"&password="+pass+"&cmd=authenticate&login=Log In";
 data = "buttonClicked=4&err_flag=0&err_msg=&info_flag=0&info_msg=&network_name=Guest+Network&username="+name+"&password="+pass;
-
-//window.alert(data);
 loadXMLDoc();
-response;
+//response;
+
+
 function loadXMLDoc(){
     var xmlhttp;
-    //if (window.XMLHttpRequest){
-      // code for IE7+, Firefox, Chrome, Opera, Safari
-      xmlhttp=new XMLHttpRequest();
-    //}
-    //else{
-      // code for IE6, IE5
-      //xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    //}
-
+    xmlhttp=new XMLHttpRequest();
     xmlhttp.open("POST",url,false);
     xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     xmlhttp.send(data);
-    //document.getElementById("main").innerHTML = xmlhttp.responseText;
-    response = xmlhttp.responseText;
-    console.log(response);
+
+    if( xmlhttp.status == 200 ){
+      document.getElementById("main").innerHTML = "<h1>Logged In <img src='img/logo.png'></h1>";
+    } else {
+      document.getElementById("main").innerHTML = "<h1>Failure while logging in</h1>";
+    }
 }
